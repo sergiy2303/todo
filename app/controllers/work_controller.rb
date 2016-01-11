bacae1a75e7591a9874ca601999a1c89963bb742
Sergiy Topchiy <sergiy2303@outlook.com>
2016-01-11 22:17:22 +0200

    Added full CRUD, some styles

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -5,7 +5,7 @@ class WorkController < ApplicationController
   end
 
   def create
-    work = Work.new(params.require(:work).permit(:description))
+    work = Work.new(work_params)
     work.user_id = current_user.id
     if work.save
       redirect_to root_path
@@ -15,14 +15,29 @@ class WorkController < ApplicationController
   end
 
   def destroy
-    work = Work.find(params[:id])
-    if work
+    unless work.new_record?
       work.destroy!
     end
     redirect_to root_path
   end
 
+  def edit
+    work = Work.find(params[:id])
+  end
+
+  def update
+    if work.update(work_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def work_params
+    params.require(:work).permit(:description)
+  end
 
   def works
     Work.where(user_id: current_user.id)
@@ -30,7 +45,7 @@ class WorkController < ApplicationController
   helper_method :works
 
   def work
-    Work.new
+    works.find_by(id: params[:id]) || Work.new
   end
   helper_method :work
 end
